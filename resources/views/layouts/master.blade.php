be4761165f35a85e55c64cc1c7752bd7cc4e9f69
esraq <esraq26552@gmail.com>
2025-01-17 03:21:46 +0600

    updated master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -137,22 +137,22 @@
   <!-- <div id="preloader"></div> -->
 
   <!-- JavaScript Libraries -->
-  <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('lib/jquery/jquery-migrate.min.js') }}"></script>
-<script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
-<script src="{{ asset('lib/mobile-nav/mobile-nav.js') }}"></script>
-<script src="{{ asset('lib/wow/wow.min.js') }}"></script>
-<script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
-<script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
-<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('lib/isotope/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('lib/lightbox/js/lightbox.min.js') }}"></script>
+  <script src="/public/lib/jquery/jquery.min.js"></script>
+<script src="/public/lib/jquery/jquery-migrate.min.js"></script>
+<script src="/public/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/public/lib/easing/easing.min.js"></script>
+<script src="/public/lib/mobile-nav/mobile-nav.js"></script>
+<script src="/public/lib/wow/wow.min.js"></script>
+<script src="/public/lib/waypoints/waypoints.min.js"></script>
+<script src="/public/lib/counterup/counterup.min.js"></script>
+<script src="/public/lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="/public/lib/isotope/isotope.pkgd.min.js"></script>
+<script src="/public/lib/lightbox/js/lightbox.min.js"></script>
 <!-- Contact Form JavaScript File -->
-<script src="{{ asset('contactform/contactform.js') }}"></script>
+<script src="/public/contactform/contactform.js"></script>
 
 <!-- Template Main Javascript File -->
-<script src="{{ asset('js/main.js') }}"></script>
+<script src="/public/js/main.js"></script>
 
 
 </body>

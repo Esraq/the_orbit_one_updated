3547534c2daeb1f56981d68725a9f358a0f6a6e9
esraq <esraq26552@gmail.com>
2025-01-17 03:13:43 +0600

    updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -137,22 +137,23 @@
   <!-- <div id="preloader"></div> -->
 
   <!-- JavaScript Libraries -->
-  <script src="{{asset('/lib/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('/lib/jquery/jquery-migrate.min.js')}}"></script>
-  <script src="{{asset('/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('/lib/easing/easing.min.js')}}"></script>
-  <script src="{{asset('/lib/mobile-nav/mobile-nav.js')}}"></script>
-  <script src="{{asset('/lib/wow/wow.min.js')}}"></script>
-  <script src="{{asset('/lib/waypoints/waypoints.min.js')}}"></script>
-  <script src="{{asset('/lib/counterup/counterup.min.js')}}"></script>
-  <script src="{{asset('/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-  <script src="{{asset('/lib/isotope/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('/lib/lightbox/js/lightbox.min.js')}}"></script>
-  <!-- Contact Form JavaScript File -->
-  <script src="{{asset('/contactform/contactform.js')}}"></script>
+  <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('lib/jquery/jquery-migrate.min.js') }}"></script>
+<script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('lib/mobile-nav/mobile-nav.js') }}"></script>
+<script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
+<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('lib/isotope/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('lib/lightbox/js/lightbox.min.js') }}"></script>
+<!-- Contact Form JavaScript File -->
+<script src="{{ asset('contactform/contactform.js') }}"></script>
 
-  <!-- Template Main Javascript File -->
-  <script src="{{asset('/js/main.js')}}"></script>
+<!-- Template Main Javascript File -->
+<script src="{{ asset('js/main.js') }}"></script>
+
 
 </body>
 </html>

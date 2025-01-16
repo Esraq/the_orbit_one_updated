7a5e74ad8134ab35a4abea7c871634587def8294
esraq <esraq26552@gmail.com>
2025-01-17 03:06:59 +0600

    master updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -137,22 +137,22 @@
   <!-- <div id="preloader"></div> -->
 
   <!-- JavaScript Libraries -->
-  <script src="{{url('/lib/jquery/jquery.min.js')}}"></script>
-  <script src="{{url('/lib/jquery/jquery-migrate.min.js')}}"></script>
-  <script src="{{url('/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{url('/lib/easing/easing.min.js')}}"></script>
-  <script src="{{url('/lib/mobile-nav/mobile-nav.js')}}"></script>
-  <script src="{{url('/lib/wow/wow.min.js')}}"></script>
-  <script src="{{url('/lib/waypoints/waypoints.min.js')}}"></script>
-  <script src="{{url('/lib/counterup/counterup.min.js')}}"></script>
-  <script src="{{url('/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-  <script src="{{url('/lib/isotope/isotope.pkgd.min.js')}}"></script>
-  <script src="{{url('/lib/lightbox/js/lightbox.min.js')}}"></script>
+  <script src="{{asset('/lib/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('/lib/jquery/jquery-migrate.min.js')}}"></script>
+  <script src="{{asset('/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('/lib/easing/easing.min.js')}}"></script>
+  <script src="{{asset('/lib/mobile-nav/mobile-nav.js')}}"></script>
+  <script src="{{asset('/lib/wow/wow.min.js')}}"></script>
+  <script src="{{asset('/lib/waypoints/waypoints.min.js')}}"></script>
+  <script src="{{asset('/lib/counterup/counterup.min.js')}}"></script>
+  <script src="{{asset('/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('/lib/isotope/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('/lib/lightbox/js/lightbox.min.js')}}"></script>
   <!-- Contact Form JavaScript File -->
-  <script src="{{url('/contactform/contactform.js')}}"></script>
+  <script src="{{asset('/contactform/contactform.js')}}"></script>
 
   <!-- Template Main Javascript File -->
-  <script src="{{url('/js/main.js')}}"></script>
+  <script src="{{asset('/js/main.js')}}"></script>
 
 </body>
 </html>

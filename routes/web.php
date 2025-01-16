2cf6c5af75be69f643ed08fb17df1b0fa38045c5
esraq <esraq26552@gmail.com>
2025-01-16 20:51:45 +0600

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SiteController;
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [SiteController::class, 'site']);
